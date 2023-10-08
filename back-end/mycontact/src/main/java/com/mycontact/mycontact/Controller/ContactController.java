344b34541bbebcd5d7b6e1309f7fa0e9076b1039
Omar Emad <64874084+OmarE505@users.noreply.github.com>
2023-10-08 21:42:08 +0300

    added Crossorigins to controller

--- back-end/mycontact/src/main/java/com/mycontact/mycontact/Controller/ContactController.java
+++ back-end/mycontact/src/main/java/com/mycontact/mycontact/Controller/ContactController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ import com.mycontact.mycontact.Service.ContactService;
 
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/v1")
 public class ContactController {
     ContactService contactService;
