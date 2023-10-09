4e0ec8cbaea67eb1ed2444c91c6f51b7b07c2c77
Omar Emad <64874084+OmarE505@users.noreply.github.com>
2023-10-09 07:29:04 +0300

    deleted Patch import

--- back-end/mycontact/src/main/java/com/mycontact/mycontact/Controller/ContactController.java
+++ back-end/mycontact/src/main/java/com/mycontact/mycontact/Controller/ContactController.java
@@ -3,7 +3,6 @@ package com.mycontact.mycontact.Controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
