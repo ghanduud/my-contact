b3db17bdc639070b66a854341203750b6e2dd221
Omar Emad <64874084+OmarE505@users.noreply.github.com>
2023-10-11 18:24:53 +0300

    Update ContactController.java

--- back-end/mycontact/src/main/java/com/mycontact/mycontact/Controller/ContactController.java
+++ back-end/mycontact/src/main/java/com/mycontact/mycontact/Controller/ContactController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -49,7 +52,7 @@ public class ContactController {
     }
 
     @DeleteMapping("/contacts/{id}")
-    public void deleteContact(@PathVariable int id) {
-        contactService.removeContact(id);
+    public ResponseEntity<String> deleteContact(@PathVariable int id) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
     }
 }
